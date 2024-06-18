e2ed0bb7ba2f373980c85d3f536dea6d80de49d5
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-18 15:08:46 -0400

    Passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finishedString = "";
+    if (num < 0) {
+        finishedString = "ERROR";
+    } else {
+    for (let i = 0; i < num; i++) {
+        finishedString += string;
+    }
+}
+    return finishedString;
 };
 
 // Do not edit below this line
