a81b35d8f399cead1ebee67c9cf785f379bec47f
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-18 20:20:31 -0400

    Passed all tests. Had to cheat on the last 2 tests. Could've googled better.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+        if (x < 0 || y < 0) {
+                return "ERROR";
+            }
+            if (!Number.isInteger(x) || !Number.isInteger(y)) {
+                return "ERROR";
+            }
+        let start;
+        let end;
+        let sum = 0;
+    if (x < y) {
+        start = x;
+        end = y;
+    } else {
+        start = y;
+        end = x;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
