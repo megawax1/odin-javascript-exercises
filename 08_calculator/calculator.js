ab0475c925a72ddfdbda231bbe4f9ae18b1150ab
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-28 14:48:50 -0400

    Passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((accumulator, next) => accumulator + next, 0)
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+  for (let i = 1; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
