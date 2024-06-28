72a3c27e6b0c7d1d6f4d8fea6d2744f33db057a4
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-28 16:53:13 -0400

    Pass

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArray = [];
+    array.forEach(element => newArray.push(element.title));
+    return newArray;
 };
 
 // Do not edit below this line
