b33037cf0ce40ac5161807de070d2e7dfbd89009
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-28 15:38:20 -0400

    Need more exposure to using filter like this.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const newWord = word
+    .toLowerCase()
+    .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+    const reversedWord = newWord.split('').reverse().join('');
+    return reversedWord === newWord;
+    
 };
 
 // Do not edit below this line
