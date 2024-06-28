fdab6b203f306f7a178edb9fb4167d118002b03a
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-06-28 16:50:58 -0400

    My code passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) { 
+        return "OOPS";
+    }
+    if (n == 0) {
+        return 0;
+    }
+    let current = 1;
+    let previous = 0;
+    let result = 1;
+    for (let i = 1; i < n; i++) {
+        result = previous + current;
+        previous = current;
+        current = result;
+        
+        
+    }
+    return result;
 };
 
 // Do not edit below this line
