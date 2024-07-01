876cbcd943ae65c04c501a0934b2c37e2547bf12
megawax1 <101356361+megawax1@users.noreply.github.com>
2024-07-01 17:04:40 -0400

    Passed... Sloppily

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(array) {
+  let sorted = array.slice(0)
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
+  let hasntDied = function(arr) {
+    for (let i = 0; i < arr.length; i++)  {
+      if (!arr[i].yearOfDeath) {
+        arr[i].yearOfDeath = currentYear;
+      }
+    }
+   }
+   hasntDied(sorted);
+   
+  sorted
+  .sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+ return sorted[0];
 };
 
 // Do not edit below this line
